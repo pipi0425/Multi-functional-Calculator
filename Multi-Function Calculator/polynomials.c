d24afc41c4ca18907ebca44bfa539b19b542e8c6
pipi0425 <2779716885@qq.com>
2019-04-17 02:43:17 -0400

    create function quadratic and begin with caller function

--- Multi-Function Calculator/polynomials.c
+++ Multi-Function Calculator/polynomials.c
@@ -1,5 +1,64 @@
 #include "polynomials.h"
+#include <math.h>
 
 void cubic(double a, double b, double c, double d) {
 
+}
+
+int quadratic(double a, double b, double c,
+	double *real1, double *real2, double *imag1, double *imag2) {
+	if (a == 0) {
+		if (b == 0) {
+			if (c == 0) {
+				return 1;
+			}
+			return 2;
+		}
+		*real1 = (-c) / b;
+		return 3;
+	}
+	double delta = b * b - 4 * a * c;
+	if (delta > 0) {
+		*real1 = ((-b) + sqrt(delta)) / (2 * a);
+		*real2 = ((-b) - sqrt(delta)) / (2 * a);
+		return 4;
+	}
+	else if (delta == 0) {
+		*real1 = (-b) / (2 * a);
+		return 5;
+	}
+	else {
+		*real1 = (-b) / (2 * a);
+		*real2 = (-b) / (2 * a);
+		*imag1 = sqrt(-delta);
+		*imag2 = -sqrt(-delta);
+		return 6;
+	}
+}
+
+void polynomial_call(void) {
+	//print header
+	for (int count = 0; count < 50; count++) {
+		printf("#");
+	}
+	printf("\n");
+	printf("This is the Polynomial Calculator (currently quadratic & cubic supported)\n");
+	for (int count = 0; count < 50; count++) {
+		printf("#");
+	}
+	printf("\n\n");
+
+	printf("Please enter 2 or 3 for quadratic equation or cubic equation, else to quit\n");
+	char type;
+	scanf_s(" %c", &type, sizeof(type));
+	if (type == '2') {
+
+	}
+	else if (type == '3') {
+
+	}
+	else {
+		return;
+	}
+	
 }
--- Multi-Function Calculator/polynomials.h
+++ Multi-Function Calculator/polynomials.h
@@ -1,3 +1,19 @@
 #pragma once
 
 void cubic(double a, double b, double c, double d);
+
+/* quadratic calculates roots for ax^2+bx+c=0
+return:
+1: a = b = c = 0: x is everything
+2: a = b = 0, c != 0: no solution
+3: a = 0: bx + c = 0: one solution
+4: delta > 0: 2 distinct real solution
+5: delta = 0: 2 equal real solution
+6: delta < 0: 2 distinct complex solution
+and modify value for real and imag
+*/
+int quadratic(double a, double b, double c,
+	double *real1, double *real2, double *imag1, double *imag2);
+
+//polynomial_call being called as a intro to switch among functions in this section
+void polynomial_call(void);
